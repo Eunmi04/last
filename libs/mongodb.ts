438bd66934d0a25b8eb476a4cecd7cdf12042025
Eunmi04 <eunmi041119@gmail.com>
2024-12-19 08:07:38 +0900

    몽고

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,10 +1,34 @@
-import mongoose from 'mongoose'
+// /libs/mongodb.ts
+import mongoose from 'mongoose';
 
-export default async function connectMongoDB() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string)
-    console.log('Connected to MongoDB')
-  } catch (error) {
-    console.log(error)
-  }
+const MONGODB_URI = process.env.MONGODB_URI || '';
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
 }
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connectMongoDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => {
+      console.log('MongoDB connected');
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectMongoDB;
